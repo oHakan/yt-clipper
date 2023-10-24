dc5caaef44ad2b5fc725e7b096333ccd137f7a60
oHakan <osmanhakan54@gmail.com>
2023-10-24 14:55:21 +0300

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
+  providers: [AppResolver, AppService],
+})
+export class AppModule {}
